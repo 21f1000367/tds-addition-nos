29fe9044158f9cfc26ad37f6460c4ca32a754274
21f1000367 <119425988+21f1000367@users.noreply.github.com>
2022-11-29 21:42:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
 
 st.write("""
 # Addition of Two Given Numbers
@@ -12,12 +11,8 @@ This app returns the sum of two given numbers
 st.header('User Input Parameters')
 number1 = st.number_input("NUMBER_1")
 number2 = st.number_input("NUMBER_2")
-   
-data = {'NUMBER_1': number1,
-        'NUMBER_2': number2
-        }
 
 add = number1 + number2
 
-st.subheader('Sum of the two given numbers')
+st.header('Sum of the two given numbers')
 st.write(add)

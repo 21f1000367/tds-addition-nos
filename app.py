181e277d15f1356d83a2b8a1c3c467429e42f4c1
21f1000367 <119425988+21f1000367@users.noreply.github.com>
2022-11-29 21:34:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
-import pickle
 
 st.write("""
 # Addition of Two Given Numbers
@@ -12,23 +10,14 @@ This app returns the sum of two given numbers
 #Get Input
 
 st.header('User Input Parameters')
-
-def user_input_features():
-    number1 = st.number_input("NUMBER_1")
-    number2 = st.number_input("NUMBER_2")
+number1 = st.number_input("NUMBER_1")
+number2 = st.number_input("NUMBER_2")
    
-    data = {'NUMBER_1': number1,
-            'NUMBER_2': number2
-            }
-    features = pd.DataFrame(data, index=[0])
-    return features
+data = {'NUMBER_1': number1,
+        'NUMBER_2': number2
+        }
 
-df = user_input_features()
-
-st.subheader('User Input parameters')
-st.write(df.to_dict())
-
-add = df['NUMBER_1'] + df['NUMBER_2']
+add = number1 + number2
 
 st.subheader('Sum of the two given numbers')
-st.write(add[0])
+st.write(add)
